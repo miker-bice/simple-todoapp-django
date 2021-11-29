f4c887ec20c160ca809fea08cd626db4c7e7d41d
miker-bice <mikejerrson.galindez@gmail.com>
2021-11-29 17:50:06 +0800

    delete function fixed
    the admin panel for the to do list is also added

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TodoListItem
+
+
+class todoappAdmin(admin.ModelAdmin):
+    list_display = ('content',)
+
 
 # Register your models here.
+admin.site.register(TodoListItem, todoappAdmin)
--- todoproject/urls.py
+++ todoproject/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('todoapp/', todoappView),
     path('addTodoItem', addTodoView),
-    path('deleteTodoItem/<int:i>', deleteTodoView)
+    path('deleteTodoItem/<int:i>/', deleteTodoView)
 ]
