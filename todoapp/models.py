033eede40ef98f928b833b7d60747dae8cf74b33
miker-bice <mikejerrson.galindez@gmail.com>
2021-11-29 17:28:00 +0800

    add task function done

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class TodoListItem(models.Model):
+    content = models.TextField()
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,15 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import TodoListItem
 
 # Create your views here.
 def todoappView(request):
-    return render(request, 'todolist.html')
+    all_todo_items = TodoListItem.objects.all()
+    return render(request, 'todolist.html', {'all_items': all_todo_items})
+
+def addTodoView(request):
+    x = request.POST['content']
+    new_item = TodoListItem(content = x)
+    new_item.save()
+    return HttpResponseRedirect('/todoapp/')
