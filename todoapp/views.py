82882edf211c7b5a9f71777004e87c743b7fc169
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-02 15:24:24 +0800

    templates structure modified
    I created a specific templates folder inside the todoapp folder to ensure flexibility

--- todoapp/views.py
+++ todoapp/views.py
@@ -6,12 +6,12 @@ from .models import TodoListItem
 # Create your views here.
 def todoappView(request):
     all_todo_items = TodoListItem.objects.all()
-    return render(request, 'todolist.html', {'all_items': all_todo_items})
+    return render(request, 'todoapp/index.html', {'all_items': all_todo_items})
 
 
 def addTodoView(request):
     x = request.POST['content']
-    new_item = TodoListItem(content = x)
+    new_item = TodoListItem(content=x)
     new_item.save()
     return HttpResponseRedirect('/todoapp/')
 
